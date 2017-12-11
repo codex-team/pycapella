8b29d50447b1eb9aae326d5f134551668e0da359
N0str <ntpcp@yandex.ru>
2017-12-12 00:23:38 +0300

    Update SDK sugar

--- pycapella/api.py
+++ pycapella/api.py
@@ -1,3 +1,4 @@
+import functools
 import json
 import os.path
 import requests
@@ -8,7 +9,7 @@ class CapellaApi:
     """
     Python SDK for Capella
     @Website: https://capella.pics
-    @Documentation: https://github.com/codex-team/capella
+    @Documentation: https://github.com/codex-team/pycapella
     """
 
     def __init__(self, config={}):
@@ -17,8 +18,10 @@ class CapellaApi:
         :param config:
         """
         self.API_URL = config.get("API_URL", "https://capella.pics/upload")
+        self.filters = []
+        self.url = ""
 
-    def uploadUrl(self, url):
+    def upload_url(self, url):
         """
         Upload remote URL to Capella
         :param url: URL for uploading to Capella
@@ -26,13 +29,13 @@ class CapellaApi:
         """
         try:
             response = requests.post(self.API_URL, {'link': url})
-            return self.processRespose(response)
+            return self.process_respose(response)
 
         except Exception as e:
             logging.error("[uploadUrl runtime error]: {}".format(e))
             return False
 
-    def uploadFile(self, file_path):
+    def upload_file(self, file_path):
         """
         Upload local file to Capella
         :param file_path: local file path for uploading to Capella
@@ -44,13 +47,13 @@ class CapellaApi:
         try:
             files = {'file': open(file_path, 'rb')}
             response = requests.post(self.API_URL, files=files)
-            return self.processRespose(response)
+            return self.process_respose(response)
 
         except Exception as e:
             logging.error("[uploadFile runtime error]: {}".format(e))
             return False
 
-    def processRespose(self, response):
+    def process_respose(self, response):
         """
         Process and return a response from Capella API
         :param python requests.response object
@@ -69,6 +72,75 @@ class CapellaApi:
             if not api_response.get("success"):
                 logging.warning("[uploadUrl] API response with error: {}".format(api_response.get("message")))
             else:
+                self.url = api_response['url']
                 logging.debug("[uploadUrl] API response with success: {}".format(api_response.get("message")))
 
             return api_response
+
+    def crop(self, width, height=None):
+        """
+        Apply crop filter to the image
+        :param width: crop width
+        :param height: crop height (optional, initially defined as width)
+        :return: self
+        """
+        self.filters.append(('crop', (width, height if height else width)))
+        return self
+
+    def resize(self, width, height=None, left=None, top=None):
+        """
+        Apply resize filter to the image
+        :param width: resized image width
+        :param height: resized image height (optional, initially defined as width)
+        :param left: resized image left (optional)
+        :param top: resized image top (optional)
+        :return: self
+        """
+        if left and top:
+            self.filters.append(('resize', (width, height if height else width, left, top)))
+        else:
+            if left or top:
+                logging.warning("[resize] You should define both left and top params")
+
+            self.filters.append(('resize', (width, height if height else width)))
+        return self
+
+    def pixelize(self, size):
+        """
+        Apply pixelize filter to the image
+        :param size: size
+        :return: self
+        """
+        self.filters.append(('pixelize', size))
+        return self
+
+    def clear(self):
+        """
+        Clear filters
+        :return: self
+        """
+        self.filters.clear()
+        return self
+
+    def get_url(self):
+        """
+        Get url of the final image
+        :return: string URL
+        """
+        url = ''
+        for filter, params in self.filters:
+            try:
+                if filter == "resize" and len(params) == 4:
+                    url += '/{}/{}x{}&{},{}'.format(filter, *params)
+                elif filter == "pixelize":
+                    url += '/{}/{}'.format(filter, params)
+                else:
+                    url += '/{}/{}x{}'.format(filter, *params)
+            except Exception as e:
+                logging.error("[upload] error: {}".format(e))
+
+        if not self.url:
+            logging.warning("[upload file before applying filters]")
+            return None
+
+        return self.url + url
--- samples/advanced.py
+++ samples/advanced.py
@@ -1,11 +1,8 @@
 from pycapella import CapellaApi
 
-
-api = CapellaApi({})
-
-
 def upload_file_example():
-    response = api.uploadFile("image.jpg")
+    api = CapellaApi()
+    response = api.upload_file("../samples/image.jpg")
     if response:
         print("Success! Image URL is {}".format(response['url']))
     else:
@@ -13,13 +10,38 @@ def upload_file_example():
 
 
 def upload_url_example():
-    response = api.uploadUrl("https://capella.pics/532117ec-750f-47eb-b4de-6db8c5ae4781")
+    api = CapellaApi()
+    response = api.upload_url("https://capella.pics/532117ec-750f-47eb-b4de-6db8c5ae4781")
     if response:
         print("Success! Image URL is {}".format(response['url']))
     else:
         print("Error!")
 
 
+def upload_file_and_apply_filters(api):
+    api.pixelize(150)
+    print("Success! Image URL is {}".format(api.get_url()))
+
+
+def crop_and_resize_image(api):
+    api.crop(400, 400)
+    api.resize(150, 150)
+    print("Success! Image URL is {}".format(api.get_url()))
+
+
+def resize_and_pixelize_in_one_line(api):
+    url = api.pixelize(150).resize(100, 100, 50, 50).resize(1000, 900).pixelize(200).get_url()
+    print("Success! Image URL is {}".format(url))
+
+
 if __name__ == "__main__":
     upload_file_example()
     upload_url_example()
+
+    new_image = CapellaApi()
+    response = new_image.upload_file("../samples/image.jpg")
+    assert response['success'] == True
+
+    upload_file_and_apply_filters(new_image)
+    crop_and_resize_image(new_image)
+    resize_and_pixelize_in_one_line(new_image)
