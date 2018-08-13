b29c7f76721e7122de9f125945bc5de5f0d50366
N0str <ntpcp@yandex.ru>
2018-08-13 21:50:15 +0300

    Fix tests structure a bit

--- tests/test_empty_filters.py
+++ tests/test_empty_filters.py
@@ -1,26 +1,32 @@
+import unittest
+
 from pycapella import Capella
 from random import randint
 
 
-def test_empty_pixelize():
-    """
-    Test valid filtering (pixelize) settings on empty image
-    """
-    api = Capella()
-    assert api.pixelize(100).filters == [('pixelize', 100)]
+class TestEmptyFilters(unittest.TestCase):
+
+    def test_empty_pixelize(self):
+        """
+        Test valid filtering (pixelize) settings on empty image
+        """
+        api = Capella()
+        assert api.pixelize(100).filters == [('pixelize', 100)]
+
+    def test_empty_random_pixelize(self):
+        """
+        Test valid filtering (pixelize) settings on empty image.
+        Random sample of {3..100} pixelize filters with argument={0..100}
+        :return:
+        """
+        api = Capella()
+        valid = []
+        for i in range(randint(3, 100)):
+            pix = randint(0, 100)
+            valid.append(('pixelize', pix))
+            api.pixelize(pix)
+        assert api.filters == valid
 
 
-def test_empty_random_pixelize():
-    """
-    Test valid filtering (pixelize) settings on empty image.
-    Random sample of {3..100} pixelize filters with argument={0..100}
-    :return:
-    """
-    api = Capella()
-    valid = []
-    for i in range(randint(3, 100)):
-        pix = randint(0, 100)
-        valid.append(('pixelize', pix))
-        api.pixelize(pix)
-    assert api.filters == valid
-
+if __name__ == '__main__':
+    unittest.main()
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,15 +1,22 @@
+import unittest
+
 from pycapella import Capella
 
 
-def test_default_url():
-    """
-    Check validity of default API URL argument
-    """
-    assert Capella().API_URL == "https://capella.pics/upload"
+class TestMain(unittest.TestCase):
+
+    def test_default_url(self):
+        """
+        Check validity of default API URL argument
+        """
+        assert Capella().API_URL == "https://capella.pics/upload"
+
+    def test_empty_get_url(self):
+        """
+        Check None result for getting url of empty image
+        """
+        assert Capella().get_url() == None
 
 
-def test_empty_get_url():
-    """
-    Check None result for getting url of empty image
-    """
-    assert Capella().get_url() == None
+if __name__ == '__main__':
+    unittest.main()
